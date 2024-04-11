b30f10d0bec77bbe04dad3773009922c331efc10
Arjen <79272976+corewebvitals@users.noreply.github.com>
2024-04-11 18:42:37 +0200

    Update resource-css-initiator.js

--- code/performance/resource-css-initiator.js
+++ code/performance/resource-css-initiator.js
@@ -1,6 +1,8 @@
 /* log all background images on a page to the console */
+const imageExtensions = ['jpg', 'jpeg', 'png', 'webp', 'avif', 'jxl', 'gif', 'svg'];
+
 let bgimg = performance.getEntriesByType('resource')
-  .filter(rs => rs.initiatorType == 'css' && rs.contentType?.startsWith('image'))
+  .filter(rs => rs.initiatorType == 'css' && (rs?.contentType?.startsWith('image') || imageExtensions.includes(rs?.name?.split('.').pop().toLowerCase())))
   .map(rs => {
     return {
       name: rs.name,
