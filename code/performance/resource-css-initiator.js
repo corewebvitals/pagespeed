110c9b75d774a222a0318511756d683c360d39dc
Joan León <joan.leon@gmail.com>
2024-04-11 09:23:59 +0200

    filter resources that are images

--- code/performance/resource-css-initiator.js
+++ code/performance/resource-css-initiator.js
@@ -1,6 +1,7 @@
 /* log all background images on a page to the console */
+let imageExtensions = ['jpg', 'jpeg', 'png', 'webp', 'avif', 'jxl', 'gif', 'svg'];
 let bgimg = performance.getEntriesByType('resource')
-  .filter(rs => rs.initiatorType == 'css')
+  .filter(rs => rs.initiatorType == 'css' && imageExtensions.includes(rs.name.split('.').pop().toLowerCase()))
   .map(rs => {
   return {
     name: rs.name,
